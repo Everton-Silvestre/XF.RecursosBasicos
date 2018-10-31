1c23da43d4a79b33bed351a7d1a0dd8c8312742c
Everton <everton.srs@gmail.com>
2018-10-30 22:03:17 -0300

    Commit Initial

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/Email.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XF.ControlesBasicos
+{
+    public class Email : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool permissao;
+        public bool Permissao
+        {
+
+            get
+            {
+                return permissao;
+            }
+            set
+            {
+                permissao = value;
+                if (!permissao) ContaEmail = string.Empty;
+                NotifyPropertyChanged();
+            }
+
+        }
+
+        private string contaEmail;
+        public string ContaEmail
+        {
+            get
+            {
+                return contaEmail;
+            }
+            set
+            {
+                contaEmail = value;
+                NotifyPropertyChanged();
+            }
+
+        }
+    }
+}
--- XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -9,9 +9,28 @@ namespace XF.ControlesBasicos
 {
     public partial class MainPage : ContentPage
     {
+        Email emailModel;
+
         public MainPage()
         {
             InitializeComponent();
+
+            if (emailModel == null) emailModel = new Email();
+
+        }               
+
+        private void Enviar_Clicked(object sender, EventArgs e)
+        {
+            if ((emailModel.Permissao) && (!string.IsNullOrEmpty(emailModel.ContaEmail)))
+                DisplayAlert("Mensagem", $"Envio feito para {emailModel.ContaEmail} ","OK");
+            else
+                DisplayAlert("Mensagem","Envio não permitido","OK");
+
+        }
+
+        private void Configuracao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage() { BindingContext = emailModel } );
         }
     }
 }
